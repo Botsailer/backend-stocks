cc2e4949bbcab90bc49ca621288cc183a309747d
HrS GOD <95730735+Botsailer@users.noreply.github.com>
2025-05-20 22:39:08 +0530

    Update bundle.js

--- models/bundle.js
+++ models/bundle.js
@@ -1,15 +1,56 @@
 const mongoose = require('mongoose');
 
 const bundleSchema = new mongoose.Schema({
-  name:        { type:String, required:true },
-  description: String,
-  models:      [{ type: mongoose.Schema.Types.ObjectId, ref:'StockModel' }],
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  portfolios: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Portfolio',
+    required: true
+  }],
+  discountPercentage: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 100,
+    default: 0
+  },
   subscription: {
-    minInvestment: { type:Number, required:true },
-    feeAmount:     { type:Number, required:true },
-    feeCurrency:   { type:String, default:'INR' },
-    feeInterval:   { type:String, enum:['one-time','monthly','yearly'], default:'one-time' }
+    minInvestment: {
+      type: Number,
+      required: true
+    },
+    feeAmount: {
+      type: Number,
+      required: true
+    },
+    feeCurrency: {
+      type: String,
+      default: 'INR'
+    },
+    feeInterval: {
+      type: String,
+      enum: ['one-time', 'monthly', 'yearly'],
+      default: 'one-time'
+    }
   }
-}, { timestamps:true });
+}, { 
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true } 
+});
+
+// Virtual for discounted price
+bundleSchema.virtual('discountedPrice').get(function() {
+  const originalPrice = this.subscription.feeAmount;
+  return originalPrice - (originalPrice * this.discountPercentage / 100);
+});
 
 module.exports = mongoose.model('Bundle', bundleSchema);
